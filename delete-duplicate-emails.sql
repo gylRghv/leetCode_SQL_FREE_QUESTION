9a65a76007e953907377da7c013ada1e81eee1a0
Raghav Goyal <32592021+gylRghv@users.noreply.github.com>
2022-08-19 00:08:01 +0100

    Adding 1 more method to delete
    Delete join

--- delete-duplicate-emails.sql
+++ delete-duplicate-emails.sql
@@ -10,3 +10,27 @@ delete from Person p2 where id not in (
     select min_id
     from min_ids 
 )
+
+
+--M2- delete join
+DELETE p1 FROM Person p1,
+    Person p2
+WHERE
+    p1.Email = p2.Email AND p1.Id > p2.Id;
+    
+    
+--Below code wouldnt work bcoz you can modify the table from which you are currenlty selecting.#
+-- DELETE FROM Person
+-- WHERE id NOT IN (SELECT MIN(id) FROM Person
+--                 GROUP BY email);
+                
+--Below will work - added extra layer
+
+--M3
+DELETE FROM Person
+WHERE id NOT IN (SELECT * 
+                 FROM ( SELECT MIN(id) 
+                       FROM Person
+                       GROUP BY email
+                      )
+                );
