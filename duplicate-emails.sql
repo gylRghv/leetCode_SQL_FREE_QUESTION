bfd9cb442026a5dfc226e0dec6ea5bf95dbdda79
Raghav Goyal <32592021+gylRghv@users.noreply.github.com>
2022-08-18 19:49:34 +0100

    removed * and added email column in count(*)

--- duplicate-emails.sql
+++ duplicate-emails.sql
@@ -3,7 +3,7 @@
 # M1
 select distinct email as Email
 from (
-    select email, count(*) as email_count
+    select email, count(email) as email_count
     from Person p
     group by 1 
 ) x
@@ -11,11 +11,17 @@ where email_count > 1;
 
 # M2
 with emails_counts as (
-    select email, count(*) as email_count
+    select email, count(email) as email_count
     from Person p
     group by 1
 )
 
 select distinct email as Email 
 from emails_counts
-where email_count > 1
+where email_count > 1;
+
+#M3
+select Email
+from Person
+group by Email
+having count(Email) > 1;
